89739ec32cbc2cbeae06ecaaa9cd5a3681b34dfa
Agrata Patel <agratapatel@gmail.com>
2023-06-12 21:10:24 +0530

    Forms, sections and text input field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("HOW ARE YOU, world!")
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your name", text: $name)
+                    Text("Hello \(name)")
+                    HStack {
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundColor(.accentColor)
+                        Text("How are you, world?")
+                    }
+                    Text("Woah")
+                    Text("Whatchu upto")
+                    Text("lol")
+                    Text("lol")
+                    Text("lol")
+                    Text("lol")
+                }
+                
+                Section {
+                    Text("Hehe")
+                    Text("Woah")
+                    Text("Whatchu upto")
+                    Text("lol")
+                    Text("lol")
+                    Text("lol")
+                    Text("lol")
+                }
+            }
+            .navigationTitle("Form")
+            //.navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        
     }
 }
 
